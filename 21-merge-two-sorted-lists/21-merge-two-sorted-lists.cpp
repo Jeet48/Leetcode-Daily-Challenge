e33fefc476099fbeb919416fbc036800d9d01a25
Jeet <91423597+Jeet48@users.noreply.github.com>
2022-05-22 21:54:17 +0530

    Time: 20 ms (8.27%), Space: 14.8 MB (44.07%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -16,21 +16,26 @@ public:
             return l2;
         }
         
-        if(l2==NULL)
+         if(l2==NULL)
         {
             return l1;
         }
         
-        if(l1->val <= l2->val)
-        {
-            l1->next = mergeTwoLists(l1->next,l2);
-            return l1;
+        
+        
+       if(l1->val > l2->val) std::swap(l1, l2);
+        ListNode* res = l1;
+        while(l1!=NULL && l2!=NULL){
+           ListNode* tmp = NULL;
+            while(l1!=NULL && l1->val <= l2->val){
+                tmp = l1;
+                l1=l1->next;
+            }
+            tmp->next = l2;
+            std::swap(l1, l2);
         }
-        else
-        {
-          l2->next = mergeTwoLists(l1,l2->next);
-            return l2;
-        }
+        
+        return res;
         
     }
 };
