f3bb390a4bf3f4402d2b49e3b87ff4b66324e7e7
Jeet <91423597+Jeet48@users.noreply.github.com>
2022-06-28 01:53:52 +0530

    Time: 1206 ms (64.60%), Space: 0B (100.00%) - LeetHub

---
+++ 607-sales-person/607-sales-person.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select s.name
+from
+salesperson s
+left join orders o on (o.sales_id=s.sales_id)
+left join company c on (o.com_id=c.com_id)
+group by s.name
+having count(order_id)=0 or not sum(c.name='RED')>0
