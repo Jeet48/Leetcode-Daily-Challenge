674ff2e980390e6fed79918d72160f8c3eb76f03
Jeet <91423597+Jeet48@users.noreply.github.com>
2022-07-12 23:27:04 +0530

    Time: 1566 ms (6.12%), Space: 40.3 MB (12.12%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int subseq(vector<int>& arr,int prev,int n, int dp[][2600], int len)
+    {
+      if(n==0)
+      {
+          return dp[prev][n] = 0;
+      }
+        if(dp[prev][n]!=-1)
+        {
+           return dp[prev][n];
+        }
+        
+        if(prev==len+1 || arr[n-1]<arr[prev-1])
+        {
+          return dp[prev][n] = max(1+subseq(arr, n, n-1, dp, len),subseq(arr, prev, n-1, dp, len));
+        }
+       return dp[prev][n] = subseq(arr, prev, n-1, dp, len);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int prev = nums.size()+1;
+        int dp[2600][2600];
+        memset(dp, -1,sizeof dp);
+        return subseq(nums,prev,nums.size(), dp, nums.size());
+    }
+};
